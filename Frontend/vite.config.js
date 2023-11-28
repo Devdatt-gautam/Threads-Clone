4c6ddd93170e9ca4e416b5418bafe630553aa4d1
Devdatt Gautam <devg26092001@gmail.com>
2023-11-28 13:50:35 +0530

    added

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Threads-Clone/",
+  base: "/Threads-Clone/Frontend",
   server: {
     port: 3000,
     //get rid of cors errors
